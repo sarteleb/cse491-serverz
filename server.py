2e14f41821abe4d0f9449fe6126b6ec19d7fbe02
Kang Suh <suhkang@arctic.cse.msu.edu>
2014-01-15 20:47:57 -0500

    homework 1

--- server.py
+++ server.py
@@ -18,6 +18,9 @@ while True:
     # Establish connection with client.    
     c, (client_host, client_port) = s.accept()
     print 'Got connection from', client_host, client_port
-    c.send('Thank you for connecting')
-    c.send("good bye.")
+    print    
+    c.send('HTTP/1.0 200 OK\r\n')
+    c.send('Content-Type: text/html\r\n\r\n')
+    c.send("<h1>Hello, world</h1> this is suhkang's Web server.")
     c.close()
+
